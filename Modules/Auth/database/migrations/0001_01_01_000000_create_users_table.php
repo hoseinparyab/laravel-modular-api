d337292514ba3829af0d5f44548e8f02ddb4702d
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-12-15 16:13:02 +0330

    User Module

--- Modules/Auth/database/migrations/0001_01_01_000000_create_users_table.php
+++ Modules/Auth/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,16 +16,13 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number')->unique();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -43,7 +40,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
